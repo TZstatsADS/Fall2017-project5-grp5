c9ee824b1a0e8b775041f4359dedbb0f3a15e1e4
XinLuoCU <xl2614@columbia.edu>
2017-12-05 16:58:16 -0500

    pop route

--- lib/preprocess_data/preprocess_data.R
+++ lib/preprocess_data/preprocess_data.R
@@ -33,11 +33,12 @@ bike$`end station latitude`=c()
 
 bike$index<-1:nrow(bike)
 Age.groups <- c("Younger than 20", "20-29", "30-39", "40-49", "50-59", "60-69", "70+" )
-bike$age.group <- cut(2017-bike$`birth year`, breaks=c(0, 20, 30, 40, 50, 60, 70, 100), labels=Age.groups)
+bike$age.group <- cut(2016-bike$`birth year`, breaks=c(0, 20, 30, 40, 50, 60, 70, 100), labels=Age.groups)
 
 time.categories <- c("Before Dawn: 12am-5am", "Early Morning: 5am-7am", "Morning: 7am-10am", 
                      "Noon: 10am-2pm","Afternoon: 2pm-4pm","Dusk: 4pm-7pm", 
                      "Night: 7pm-10pm","Late Night: 10pm-12am" )
+
 hour<-as.numeric(format(bike$starttime,'%H'))
 bike$hour <- cut(hour, breaks=c(0, 5, 7, 10, 14, 16, 19, 22, 24), labels=time.categories )
 
@@ -49,11 +50,11 @@ weekend_group<-split(bike[,c("index")],bike$weekend)
 gender_group<-split(bike[,c("index")],bike$gender)
 
 
-cate2<-c()
-x=432
+cate2<-cate
+x=648
 inter.list2<-list()
-# for(i in "2"){
-  # if(i=="All"){a1=1:nrow(bike)}else{a1=gender_group[[i]]$index}
+for(i in c("All","1","2")){
+if(i=="All"){a1=1:nrow(bike)}else{a1=gender_group[[i]]$index}
   a1=gender_group[["2"]]$index
   for(j in c("All",Age.groups)){
     if(j=="All"){a2=1:nrow(bike)}else{a2=age_group[[j]]$index}
@@ -68,20 +69,51 @@ inter.list2<-list()
         cate2<-rbind(cate2,c(i,j,k,m,x))
         b1=intersect(a1,a2)
         b2=intersect(a3,a4)
-        inter.list2[[as.character(x)]]=intersect(b1,b2)
+        inter.list[[as.character(x)]]=intersect(b1,b2)
       }
     }
   }
-# }
-cate[z,]<-cate2
+}
+#cate[z,]<-cate2
 save(inter.list,cate,file="~/Desktop/[ADS]Advanced Data Science/fall2017-project5-group5/data/intersect_list.RData")
 
 ###delete the rows from bike 
 bike1<-bike[bike$route.id%in%names(all_routes),]
 #bike_routes<-bike1
 
+#### find popular station and routes
+top100st<-sort(table(bike$`start station id`),decreasing = T)[1:100]
+top100.df<-bike[bike$`start station id`%in%names(top100st),c("start station id","end station id")]
+#top100.df<-top100.df%>%group_by(`start station id`,`end station id`)%>%summarise(Freq=n())
+top100.tb<-table(top100.df$`start station id`,top100.df$`end station id`)
+sum(rownames(top100.tb)%in%names(top100st))
+df.pop100<-data.frame(start=NULL,end=NULL,Freq=NULL,route=NULL)
+for(i in 1:100){
+  start<-rownames(top100.tb)[i]
+  vec=top100.tb[i,]
+  top200<-order(vec,decreasing = T)[1:200]
+  df.pop<-data.frame(start=rep(start,200),end=colnames(top100.tb)[top200],Freq=vec[top200])
+  df.pop$route<-paste0("S.",df.pop$start,"_to_S.",df.pop$end,sep="")
+  df.pop100<-rbind(df.pop100,df.pop)
+}
+sum(df.pop100$route%in%names(all_routes))
+length(unique(df.pop100$start))
+sum(df.pop100$start%in%names(top100st))
+
+df.popular<-df.pop100
 ### popular stations
+df.popular<-df.popular[df.popular$route%in%names(all_routes),]
 popular_st<-unique(df.popular$start)
 pop_stations_info<-stations_info[stations_info$id%in%popular_st,]
-df.popular$route<-paste0("S.",df.popular$start,"_to_S.",df.popular$stop,sep="")
+Freq<-table(bike$`start station id`[bike$`start station id`%in%popular_st])
+sort(Freq)
+pop_stations_info$Freq<-NA
+for(i in 1:100){
+  pop_stations_info$Freq[i]<-Freq[names(Freq)==as.character(pop_stations_info$id[i])]
+}
+# df.popular$route<-paste0("S.",df.popular$start,"_to_S.",df.popular$stop,sep="")
+save(df.popular,pop_stations_info,file="~/Desktop/[ADS]Advanced Data Science/fall2017-project5-group5/data/popular_route.RData")
+
+
+
 
